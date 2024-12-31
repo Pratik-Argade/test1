0f82b2351a7c48e5e757f1b434e8c897313a6e2c
Pratik Argade <163032419+Pratik-Argade@users.noreply.github.com>
2024-12-31 18:17:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+# create a AWS EC2 instance using the terraform
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "testinstance2" {
+  ami           = "ami-021e165d8c4ff761d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "testinstance2"
+  }
+}
